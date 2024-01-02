0cdc80f288413600b5e2e9ddcc79a5b7c68cab84
paul <pvnick@gmail.com>
2024-01-02 18:18:16 -0500

    promotion script

---
+++ promote.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# ensure we have rsync
+sudo apt-get install -y rsync
+
+# dump database
+sudo wp search-replace hogtown-dev.paulnickerson.dev hogtowncatholic.org wp_options wp_snippets --export > database/db.sql
+
+# sync content. handle uploads separately because when we sync to prod we want to add upload files, not delete anything in the target uploads folder
+rsync -av --delete --exclude="uploads/" /bitnami/wordpress/wp-content/ wp-content-no-uploads/
+# only sync file uploads to the main wordpress, but ignore plugin-associated uploads
+rsync -av --prune-empty-dirs /bitnami/wordpress/wp-content/uploads/20* wp-uploads/
