b4d57af2807514640484a145cfa7ab3e92f892d1
paul <pvnick@gmail.com>
2024-01-02 19:19:35 -0500

    chown

--- promote.sh
+++ promote.sh
@@ -9,6 +9,6 @@ sudo apt-get install -y rsync
 sudo wp db import database/db.sql
 
 # sync content. handle uploads separately because when we sync to prod we want to add upload files, not delete anything in the target uploads folder
-rsync -av --delete --exclude="uploads/" wp-content-no-uploads/ /bitnami/wordpress/wp-content/ 
+sudo rsync -av --delete --exclude="uploads/" --chown=bitnami:daemon wp-content-no-uploads/ /bitnami/wordpress/wp-content/ 
 # only sync file uploads to the main wordpress, but ignore plugin-associated uploads
-rsync -av --prune-empty-dirs wp-uploads/ /bitnami/wordpress/wp-content/uploads/
+sudo rsync -av --prune-empty-dirs --chown=daemon:daemon wp-uploads/ /bitnami/wordpress/wp-content/uploads/
