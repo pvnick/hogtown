15741a632c02e8edaf32f80055ba19088e101b56
Paul Nickerson <pvnick@gmail.com>
2025-06-05 17:07:21 -0400

    delete everything, start over

--- docker/custom-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo $WORDPRESS_DATABASE_HOST
-apt-get update
-apt-get install -y dnsutils
-dig $WORDPRESS_DATABASE_HOST
--- wp-content-no-uploads/plugins/burst-statistics/dashboard-widget/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '174aa03f1bcc9ab7cc07');
